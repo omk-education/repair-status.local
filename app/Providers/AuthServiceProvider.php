e2e8813238cf31bd90fbd39d7c5ff721aba927e2
Leonov Ivan <it.omk@yandex.ru>
2019-06-14 08:45:10 +0300

    Создание шлюзов - администратор - продавец-консультант - мастер по ремонту

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,25 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin', function ($user) {
+            if ($user->role == 'admin') {
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('seller', function ($user) {
+            if ($user->role == 'seller') {
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('master', function ($user) {
+            if ($user->role == 'master') {
+                return true;
+            }
+            return false;
+        });
     }
 }
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,9 +1,11 @@
 @auth
+    @can('admin')
     <li class="nav-item active">
         <a class="nav-link" href="{{ route('admin.users.index') }}">
             Пользователи
         </a>
     </li>
+    @endcan
     <li class="nav-item active">
         <a class="nav-link" href="{{ route('admin.orders.index') }}">
             Заказы
